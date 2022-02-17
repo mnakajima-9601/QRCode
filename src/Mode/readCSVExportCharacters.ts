772847da18b48c2e52b503f9ea3235d2f952868d
Moe Nakajima <m.nakajima@ookinagorira.local>
2022-02-18 00:13:47 +0900

    文字入力

---
+++ src/Mode/readCSVExportCharacters.ts
@@ -0,0 +1,32 @@
+exports.readCSVExportCharacters = (destination: String) => {
+  // パラメータチェック
+  const checkParameter = require('../Check/parameter')
+  // ディレクトリ存在チェック
+  const checkExist = require('../Check/exist')
+  //　ファイル読み込み
+  const readFile = require('../File/readFile')
+  //　配列を作成
+  const toArray = require('../Create/texttoArray')
+  //　QR作成()
+  const createQrCode = require('../Create/qrCodeExportCharacters')
+  //　保存名作成
+  const saveName = require('../Create/saveName')
+  //　CSVファイル
+  const csv = checkParameter.checkParameter(process.argv[4], '読み込みファイルパス')
+
+  // ファイル存在チェック
+  if (!checkExist.checkExsit(csv)) {
+    throw '読み込みファイルが存在しません'
+  }
+  // ファイルを読み込む
+  const file = readFile.outputText(csv)
+  // 配列に変換する
+  const array = toArray.outputArray(file)
+  // 配列を一行ずつ回して処理する
+  for (var idx in array) {
+    // 保存名称を設定する
+    const name = saveName.createNameMode1(csv, array[idx], idx)
+    // QRコード画像
+    createQrCode.createQrCcode(array[idx], name, destination)
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,51 @@
 // パラメータチェック
-const checkParameter = require("./Check/parameter");
+const checkParameter = require('./Check/parameter')
 // ディレクトリ存在チェック
-const checkExist = require("./Check/exist");
+const checkExist = require('./Check/exist')
 // 権限チェック
-const checkAccess = require("./Check/access");
+const checkAccess = require('./Check/access')
 // ファイル読み込みモード
-const mode1 = require("./Mode/readCSV");
+const mode1 = require('./Mode/readCSV')
 // 文字列モード
-const mode2 = require("./Mode/readTXT");
+const mode2 = require('./Mode/readTXT')
 // IDモード
-const mode3 = require("./Mode/readID");
+const mode3 = require('./Mode/readID')
 // UUIDモード(CSV)
-const mode4 = require("./Mode/readUuidCsv");
-//　１(ファイル読み込み)、２(文字列)、３(ID)、４(UUID付与CSV)
-const mode = checkParameter.checkParameter(process.argv[2], "モード");
+const mode4 = require('./Mode/readUuidCsv')
+// ファイル読み込み　文字列書き出しモード
+const mode5 = require('./Mode/readCSVExportCharacters')
+//　１(ファイル読み込み)、２(文字列)、３(ID)、４(UUID付与CSV)、５(文字列書き出し)
+const mode = checkParameter.checkParameter(process.argv[2], 'モード')
 //　保存先
-const destination = checkParameter.checkParameter(process.argv[3], "保存先");
+const destination = checkParameter.checkParameter(process.argv[3], '保存先')
 
 // 保存先チェック
 if (!checkExist.checkExsit(destination)) {
   // フォルダがない場合は作成する
-  console.log("保存先のフォルダがありません");
-  const directory = require("./Create/directory");
-  directory.createDirectory(destination);
+  console.log('保存先のフォルダがありません')
+  const directory = require('./Create/directory')
+  directory.createDirectory(destination)
 }
 // 権限チェック
-checkAccess.checkAccess(destination);
+checkAccess.checkAccess(destination)
 
 // 処理開始
 switch (mode) {
-  case "1":
-    mode1.readCSV(destination);
-    break;
-  case "2":
-    mode2.readTXT(destination);
-    break;
-  case "3":
-    mode3.readID(destination);
-    break;
-  case "4":
-    mode4.readUuidCsv(destination);
-    break;
+  case '1':
+    mode1.readCSV(destination)
+    break
+  case '2':
+    mode2.readTXT(destination)
+    break
+  case '3':
+    mode3.readID(destination)
+    break
+  case '4':
+    mode4.readUuidCsv(destination)
+    break
+  case '5':
+    mode5.readCSVExportCharacters(destination)
+    break
   default:
-    throw "1(ファイル読み込み)、2(文字列)、3(ID)、4(UUID付与CSV)、5(文字列書き出し)を選択してください。";
+    throw '1(ファイル読み込み)、2(文字列)、3(ID)、4(UUID付与CSV)、5(文字列書き出し)を選択してください。'
 }
